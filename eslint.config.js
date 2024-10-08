0f2ab61a334a9cc2db1b351dad18e4c808955bd8
Rares Macovei <rares_mac1@yahoo.com>
2024-10-08 16:34:27 +0200

    eslint.config.js migration

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: 'eslint:recommended',
+    env: {
+      browser: true,
+    },
+    rules: {
+      semi: ['error', 'always'],
+      indent: ['error', 2],
+    },
+  };
