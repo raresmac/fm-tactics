0f82c8150657f0de74dbb8102a2d026d6dc629b2
Rares Macovei <rares_mac1@yahoo.com>
2024-10-08 18:07:13 +0200

    eslint config 2

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,8 @@
-const { configs } = require('@eslint/js');
-
-module.exports = [
-  configs.recommended,
-  {
-    env: {
-      browser: true,
-    },
-    rules: {
-      semi: ['error', 'always'],
-      indent: ['error', 2],
-      'no-unused-vars': 'warn',
-    },
-  },
+export default [ 
+  { 
+    rules: { 
+      semi: "error", 
+      "prefer-const": "error" 
+    } 
+  } 
 ];
