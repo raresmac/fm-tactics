7091e1e9ba43a0370bd9519321213b27d9783788
Rares Macovei <rares_mac1@yahoo.com>
2024-10-09 21:56:37 +0200

    added pa11y and some downloadable tactics

---
+++ pa11y.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // The URL of your Jekyll GitHub page
+    url: 'https://raresmac.github.io/fm-tactics/',
+  
+    // The paths to test
+    paths: [
+      '/fm24/tactics/beyond'
+    ],
+  
+    // The standards to test against
+    standards: ['WCAG2A', 'WCAG2AA', 'WCAG2AAA'],
+  
+    // The level of logging to display
+    log: {
+      debug: false,
+      info: true,
+      error: true,
+    },
+  };
